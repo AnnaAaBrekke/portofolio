4e9c0cc827830eab71ab49c078065c26c7627fad
AnnaAaBrekke <annabrekke98@gmail.com>
2024-10-23 09:09:17 +0200

    eslint and pretter configured together

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,22 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-
+import prettier from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
   pluginJs.configs.recommended,
+  {
+    rules: {
+      ...prettier.rules,
+      "prettier/prettier": "error",
+    },
+    plugins: {
+      prettier: prettierPlugin,
+    },
+  },
 ];
