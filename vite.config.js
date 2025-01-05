6de3b301401ace9150575c228e45c7ae23e67dbd
AnnaAaBrekke <annabrekke98@gmail.com>
2025-01-05 11:21:25 +0100

    Dist folder adjustments

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,15 @@ import autoprefixer from "autoprefixer";
 
 export default defineConfig({
   css: {
+    preprocessorOptions: {
+      scss: {},
+    },
     postcss: {
       plugins: [tailwindcss(), autoprefixer()],
     },
+  },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets", // Ensure CSS/JS/images go to 'dist/assets'
   },
 });
