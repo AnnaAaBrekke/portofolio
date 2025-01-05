acf7f9eb1dc48adea05b57db6fa8136aed415a80
AnnaAaBrekke <annabrekke98@gmail.com>
2025-01-05 12:03:53 +0100

    Build process do not inclde css files in dist folder..

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,12 @@ export default defineConfig({
   build: {
     outDir: "dist",
     assetsDir: "assets",
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name]-[hash][extname]",
+        entryFileNames: "assets/[name]-[hash].js",
+        chunkFileNames: "assets/[name]-[hash].js",
+      },
+    },
   },
 });
